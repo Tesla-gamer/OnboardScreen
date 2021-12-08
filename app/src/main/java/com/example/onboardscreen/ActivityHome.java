dd51da8614fea7ced4227f997b4b08ab8767879e
Abhishek <guptaabhishek46053@gmail.com>
2021-12-08 13:57:07 +0530

    OnBoardScreen

---
+++ app/src/main/java/com/example/onboardscreen/ActivityHome.java
@@ -0,0 +1,14 @@
+package com.example.onboardscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
